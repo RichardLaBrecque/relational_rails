90388ca3fd9e563421f093f203aed70ee20758d0
Richard LaBrecque <richardlabreque@gmail.com>
2022-02-02 16:35:11 -0500

    feat: migrations for client and adding clients to contractors

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_01_211901) do
+ActiveRecord::Schema.define(version: 2022_02_02_211429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.string "name"
+    t.boolean "recent_visit"
+    t.integer "required_visits_per_year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "contractor_id"
+    t.index ["contractor_id"], name: "index_clients_on_contractor_id"
+  end
 
   create_table "contractors", force: :cascade do |t|
     t.string "name"
     t.boolean "licensed"
     t.integer "service_cost"
     t.integer "service_area"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "clients", "contractors"
 end
