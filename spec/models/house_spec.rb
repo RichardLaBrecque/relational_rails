1bdb6bbf2a6d727518633ce9150b18037b3e0073
Tamara Dowis <tamara.dowis@gmail.com>
2022-02-04 19:35:05 -0600

    test: neighborhoods houses index

--- spec/models/house_spec.rb
+++ spec/models/house_spec.rb
@@ -3,16 +3,10 @@ RSpec.describe House, type: :model do
   let(:hood_1) {Neighborhood.create!(name: 'Happy', has_pool: true, number_of_streets: 17)}
 
   it {should belong_to :neighborhood}
-  
+
   it 'exists' do
     house_1 = hood_1.houses.create!(family_name: 'Hernandez', hos_member: false, parking_spaces: 2, neighborhood: hood_1)
    expect(house_1).to be_a(House)
   end
 
 end
-# t.string :name
-# t.boolean :hos_member
-# t.integer :parking_spaces
-# t.references :neighborhood
-# t.datetime :created_at
-# t.datetime :updated_at
