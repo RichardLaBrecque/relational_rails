b7d81d883af672352092d8da4438795fe48a94eb
Tamara Dowis <tamara.dowis@gmail.com>
2022-02-04 18:04:50 -0600

    test: association testing

--- spec/models/house_spec.rb
+++ spec/models/house_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 RSpec.describe House, type: :model do
   let(:hood_1) {Neighborhood.create!(name: 'Happy', has_pool: true, number_of_streets: 17)}
 
+  it {should belong_to :neighborhood}
+  
   it 'exists' do
-    house_1 = hood_1.houses.create!(name: 'Hernandez', hos_member: false, parking_spaces: 2, neighborhood: hood_1)
+    house_1 = hood_1.houses.create!(family_name: 'Hernandez', hos_member: false, parking_spaces: 2, neighborhood: hood_1)
    expect(house_1).to be_a(House)
   end
 
--- spec/models/neighborhood_spec.rb
+++ spec/models/neighborhood_spec.rb
@@ -1,18 +1,12 @@
 require 'rails_helper'
 RSpec.describe Neighborhood, type: :model do
+
+  it {should have_many :houses}
+
   it 'exists' do
     hood_1 = Neighborhood.create!(name: 'Happy', has_pool: true, number_of_streets: 17)
     expect(hood_1).to be_a(Neighborhood)
   end
 
-  # describe "validations" do
-  #   it {should validate_presense_of :name}
-  # end
 
 end
-# create_table :neighborhoods do |t|
-#   t.string :name
-#   t.boolean :has_pool
-#   t.integer :number_of_streets
-#   t.datetime :created_at
-#   t.datetime :updated_at
