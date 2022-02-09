50fe5649eaaf503920d14da5f0291babc03c020b
Richard LaBrecque <richardlabreque@gmail.com>
2022-02-09 11:56:40 -0500

    feat: user story 14

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -6,4 +6,21 @@ class ClientsController < ApplicationController
   def show
     @client = Client.find(params[:id])
   end
+
+  def edit
+    @client = Client.find(params[:id])
+  end
+
+  def update
+      client = Client.find(params[:id])
+      client.update({
+        name: params[:name],
+        recent_visit: params[:recent_visit],
+        required_visits_per_year: params[:required_visits_per_year],
+        })
+        binding.pry
+
+        client.save
+        redirect_to '/contractors'
+  end
 end
