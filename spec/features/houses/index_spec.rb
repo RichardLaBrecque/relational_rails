ec2ed514c281240b5d73721cf0e1f11631966112
Tamara Dowis <tamara.dowis@gmail.com>
2022-02-09 13:34:35 -0600

    test: update houses link from houses index and hood houses index

--- spec/features/houses/index_spec.rb
+++ spec/features/houses/index_spec.rb
@@ -80,5 +80,44 @@ RSpec.describe 'Houses #index', type: :feature do
     expect(page).not_to have_content(@lemon.family_name)
 
   end
+end
+RSpec.describe 'Update Neighborhood', type: :feature do
+describe 'Update a neighborhood' do
+    describe 'I can visit the neighborhood update form by clicking a link on the neighborhood#index' do
 
+    before :each do
+      @hood_1 = Neighborhood.create!(name: 'Happy', has_pool: true, number_of_streets: 17)
+      @hood_2 = Neighborhood.create!(name: 'Sad', has_pool: false, number_of_streets: 13)
+      @hood_3 = Neighborhood.create!(name: 'Medium', has_pool: true, number_of_streets: 11)
+      @hood_4 = Neighborhood.create!(name: 'Excited', has_pool: true, number_of_streets: 67)
+      @hood_5 = Neighborhood.create!(name: 'Meh', has_pool: false, number_of_streets: 3)
+
+     @dowis = @hood_1.houses.create!(family_name: 'Dowis', hos_member: false, parking_spaces: 3)
+     @ammon = @hood_1.houses.create!(family_name: 'Ammon', hos_member: true, parking_spaces: 1)
+     @fleur = @hood_1.houses.create!(family_name: 'Fleur', hos_member: true, parking_spaces: 2)
+     @sasha = @hood_2.houses.create!(family_name: 'Sasha', hos_member: false, parking_spaces: 2)
+     @john = @hood_2.houses.create!(family_name: 'John', hos_member: true, parking_spaces: 3)
+     @tamara = @hood_3.houses.create!(family_name: 'Tamara', hos_member: false, parking_spaces: 3)
+     @ann = @hood_4.houses.create!(family_name: 'Ann', hos_member: true, parking_spaces: 1)
+     @tenzin = @hood_4.houses.create!(family_name: 'Tenzin', hos_member: false, parking_spaces: 1)
+     @sam = @hood_5.houses.create!(family_name: 'Sam', hos_member: false, parking_spaces: 3)
+     @lemon = @hood_5.houses.create!(family_name: 'Lemon', hos_member: false, parking_spaces: 2)
+    end
+
+    it 'can update an existing neighborhood' do
+      visit '/houses'
+
+      click_link "Update #{@dowis.family_name} Information"
+      expect(current_path).to eq("/houses/#{@dowis.family_name}/edit")
+    end
+
+    xit 'can update an existing neighborhood' do
+      visit "/neighborhoods/#{@hood_2}/houses"
+
+      click_link "Update #{@sasha.family_name} Information"
+      expect(current_path).to eq("/houses/#{@sasha.family_name}/edit")
+
+    end
+  end
+end
 end
