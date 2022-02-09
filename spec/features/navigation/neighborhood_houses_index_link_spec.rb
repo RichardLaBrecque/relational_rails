3a63283fe7a8cdf94635676e6d0a3c01fe0820f1
Tamara Dowis <tamara.dowis@gmail.com>
2022-02-09 12:45:09 -0600

    feat: houses sort with link in neigh index

--- spec/features/navigation/neighborhood_houses_index_link_spec.rb
+++ spec/features/navigation/neighborhood_houses_index_link_spec.rb
@@ -22,89 +22,88 @@ RSpec.describe 'Neighboorhood houses #index', type: :feature do
     @hood_4 = Neighborhood.create!(name: 'Excited', has_pool: true, number_of_streets: 67)
     @hood_5 = Neighborhood.create!(name: 'Meh', has_pool: false, number_of_streets: 3)
 
-   @dowis = @hood_1.houses.create!(family_name: 'Dowis', hos_member: false, parking_spaces: 3)
-   @ammon = @hood_1.houses.create!(family_name: 'Ammon', hos_member: true, parking_spaces: 1)
-   @fleur = @hood_1.houses.create!(family_name: 'Fleur', hos_member: true, parking_spaces: 2)
-   @sasha = @hood_2.houses.create!(family_name: 'Sasha', hos_member: false, parking_spaces: 2)
-   @john = @hood_2.houses.create!(family_name: 'John', hos_member: true, parking_spaces: 3)
-   @tamara = @hood_3.houses.create!(family_name: 'Tamara', hos_member: false, parking_spaces: 3)
-   @ann = @hood_4.houses.create!(family_name: 'Ann', hos_member: true, parking_spaces: 1)
-   @tenzin = @hood_4.houses.create!(family_name: 'Tenzin', hos_member: false, parking_spaces: 1)
-   @sam = @hood_5.houses.create!(family_name: 'Sam', hos_member: false, parking_spaces: 3)
-   @lemon = @hood_5.houses.create!(family_name: 'Lemon', hos_member: false, parking_spaces: 2)
+    @dowis = @hood_1.houses.create!(family_name: 'Dowis', hos_member: false, parking_spaces: 3)
+    @ammon = @hood_1.houses.create!(family_name: 'Ammon', hos_member: true, parking_spaces: 1)
+    @fleur = @hood_1.houses.create!(family_name: 'Fleur', hos_member: true, parking_spaces: 2)
+    @sasha = @hood_2.houses.create!(family_name: 'Sasha', hos_member: false, parking_spaces: 2)
+    @john = @hood_2.houses.create!(family_name: 'John', hos_member: true, parking_spaces: 3)
+    @tamara = @hood_3.houses.create!(family_name: 'Tamara', hos_member: false, parking_spaces: 3)
+    @ann = @hood_4.houses.create!(family_name: 'Ann', hos_member: true, parking_spaces: 1)
+    @tenzin = @hood_4.houses.create!(family_name: 'Tenzin', hos_member: false, parking_spaces: 1)
+    @sam = @hood_5.houses.create!(family_name: 'Sam', hos_member: false, parking_spaces: 3)
+    @lemon = @hood_5.houses.create!(family_name: 'Lemon', hos_member: false, parking_spaces: 2)
   end
 
   it 'will link to the /neighborhoods/#{neighborhood.id/houses page from /neighborhoods/#{neighborhood.id' do
     visit "/neighborhoods/#{@hood_1.id}"
     click_on "Indexed Houses"
-   expect(current_path).to eq("/neighborhoods/#{@hood_1.id}/houses")
+    expect(current_path).to eq("/neighborhoods/#{@hood_1.id}/houses")
   end
 
   it 'will link to the /neighborhoods/#{neighborhood.id/houses page from /neighborhoods/#{neighborhood.id' do
     visit "/neighborhoods/#{@hood_2.id}"
     click_on "Indexed Houses"
-   expect(current_path).to eq("/neighborhoods/#{@hood_2.id}/houses")
+    expect(current_path).to eq("/neighborhoods/#{@hood_2.id}/houses")
   end
 
   it 'will link to the /neighborhoods/#{neighborhood.id/houses page from /neighborhoods/#{neighborhood.id' do
     visit "/neighborhoods/#{@hood_3.id}"
     click_on "Indexed Houses"
-   expect(current_path).to eq("/neighborhoods/#{@hood_3.id}/houses")
+    expect(current_path).to eq("/neighborhoods/#{@hood_3.id}/houses")
   end
 
   it 'will link to the /neighborhoods/#{neighborhood.id/houses page from /neighborhoods/#{neighborhood.id' do
     visit "/neighborhoods/#{@hood_4.id}"
     click_on "Indexed Houses"
-   expect(current_path).to eq("/neighborhoods/#{@hood_4.id}/houses")
+    expect(current_path).to eq("/neighborhoods/#{@hood_4.id}/houses")
   end
 
   it 'will link to the /neighborhoods/#{neighborhood.id/houses page from /neighborhoods/#{neighborhood.id' do
     visit "/neighborhoods/#{@hood_5.id}"
     click_on "Indexed Houses"
-   expect(current_path).to eq("/neighborhoods/#{@hood_5.id}/houses")
+    expect(current_path).to eq("/neighborhoods/#{@hood_5.id}/houses")
   end
 end
+
+require 'rails_helper'
 RSpec.describe 'Update Neighborhood', type: :feature do
-describe 'Sort Neighborhood Houses Index alphabetically' do
+  describe 'Sort Neighborhood Houses Index alphabetically' do
     describe 'I can visit the Neighborhood Houses Index and  click on a link to sort them' do
 
-    before :each do
-      @hood_1 = Neighborhood.create!(name: 'Happy', has_pool: true, number_of_streets: 17)
-      @hood_2 = Neighborhood.create!(name: 'Sad', has_pool: false, number_of_streets: 13)
-      @hood_3 = Neighborhood.create!(name: 'Medium', has_pool: true, number_of_streets: 11)
-      @hood_4 = Neighborhood.create!(name: 'Excited', has_pool: true, number_of_streets: 67)
-      @hood_5 = Neighborhood.create!(name: 'Meh', has_pool: false, number_of_streets: 3)
+      before :each do
+        @hood_1 = Neighborhood.create!(name: 'Happy', has_pool: true, number_of_streets: 17)
+        @hood_2 = Neighborhood.create!(name: 'Sad', has_pool: false, number_of_streets: 13)
+        @hood_3 = Neighborhood.create!(name: 'Medium', has_pool: true, number_of_streets: 11)
+        @hood_4 = Neighborhood.create!(name: 'Excited', has_pool: true, number_of_streets: 67)
+        @hood_5 = Neighborhood.create!(name: 'Meh', has_pool: false, number_of_streets: 3)
 
-     @dowis = @hood_1.houses.create!(family_name: 'Dowis', hos_member: false, parking_spaces: 3)
-     @ammon = @hood_1.houses.create!(family_name: 'Ammon', hos_member: true, parking_spaces: 1)
-     @fleur = @hood_1.houses.create!(family_name: 'Fleur', hos_member: true, parking_spaces: 2)
-     @sasha = @hood_2.houses.create!(family_name: 'Sasha', hos_member: false, parking_spaces: 2)
-     @john = @hood_2.houses.create!(family_name: 'John', hos_member: true, parking_spaces: 3)
-     @tamara = @hood_3.houses.create!(family_name: 'Tamara', hos_member: false, parking_spaces: 3)
-     @ann = @hood_4.houses.create!(family_name: 'Ann', hos_member: true, parking_spaces: 1)
-     @tenzin = @hood_4.houses.create!(family_name: 'Tenzin', hos_member: false, parking_spaces: 1)
-     @sam = @hood_5.houses.create!(family_name: 'Sam', hos_member: false, parking_spaces: 3)
-     @lemon = @hood_5.houses.create!(family_name: 'Lemon', hos_member: false, parking_spaces: 2)
-    end
+        @dowis = @hood_1.houses.create!(family_name: 'Dowis', hos_member: false, parking_spaces: 3)
+        @ammon = @hood_1.houses.create!(family_name: 'Ammon', hos_member: true, parking_spaces: 1)
+        @fleur = @hood_1.houses.create!(family_name: 'Fleur', hos_member: true, parking_spaces: 2)
+        @sasha = @hood_2.houses.create!(family_name: 'Sasha', hos_member: false, parking_spaces: 2)
+        @john = @hood_2.houses.create!(family_name: 'John', hos_member: true, parking_spaces: 3)
+        @tamara = @hood_3.houses.create!(family_name: 'Tamara', hos_member: false, parking_spaces: 3)
+        @ann = @hood_4.houses.create!(family_name: 'Ann', hos_member: true, parking_spaces: 1)
+        @tenzin = @hood_4.houses.create!(family_name: 'Tenzin', hos_member: false, parking_spaces: 1)
+        @sam = @hood_5.houses.create!(family_name: 'Sam', hos_member: false, parking_spaces: 3)
+        @lemon = @hood_5.houses.create!(family_name: 'Lemon', hos_member: false, parking_spaces: 2)
+      end
 
-    it 'can sort Houses Index alphabetically' do
-      visit "/neighborhoods/#{@hood_5.id}/houses"
+      it 'can sort Houses Index alphabetically' do
+        visit "/neighborhoods/#{@hood_1.id}/houses"
+
+        expect(@dowis.family_name).to appear_before(@ammon.family_name)
+        expect(@ammon.family_name).to appear_before(@fleur.family_name)
+
+        click_link 'Sort houses in alphabetical order'
 
 
-      click_link 'Sort Houses in alphabetical order'
+        expect(current_path).to eq("/neighborhoods/#{@hood_1.id}/houses")
 
-      expect(current_path).to eq("/neighborhoods/#{@hood_5.id}/houses")
+        expect(@ammon.family_name).to appear_before(@dowis.family_name)
+        expect(@dowis.family_name).to appear_before(@fleur.family_name)
 
-      expect(@ammon.family_name).to appear_before(@ann.family_name)
-      expect(@ann.family_name).to appear_before(@dowis.family_name)
-      expect(@dowis.family_name).to appear_before(@fleur.family_name)
-      expect(@fleur.family_name).to appear_before(@john.family_name)
-      expect(@john.family_name).to appear_before(@lemon.family_name)
-      expect(@lemon.family_name).to appear_before(@sam.family_name)
-      expect(@sam.family_name).to appear_before(@sasha.family_name)
-      expect(@sasha.family_name).to appear_before(@tamara.family_name)
-      expect(@tamara.family_name).to appear_before(@tenzin.family_name)
+      end
     end
   end
-end
 end
